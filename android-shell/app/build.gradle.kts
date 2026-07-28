9482353cdf86b7e2142b7ba37e994c5371ac04b6
TIMER-err <zqq2333@outlook.com>
2026-07-28 23:49:02 +0800

    release: prepare v0.9.1

--- android-shell/app/build.gradle.kts
+++ android-shell/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "dev.t1m3.qplayer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 51
-        versionName = "0.9.0"
+        versionCode = 52
+        versionName = "0.9.1"
         manifestPlaceholders["appLabel"] = "QPlayer"
     }
 
